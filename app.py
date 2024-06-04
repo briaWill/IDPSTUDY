51ade617792a4fda78473614984ee8a91fbb00e5
briaWill <36280143+briaWill@users.noreply.github.com>
2024-06-03 20:04:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def hello():
     return "Hello, World! This is the landing page for ClearImpact, Revolutionizing ESG Disclosures."
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+    app.run(port=int(os.environ.get('PORT', 8080)),host='0.0.0.0',debug=True)
 
 
